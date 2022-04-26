2c8372a2435c761a6de1fec0c99f63928409759c
Bassem <bassem@MacBook-Pro-de-Bassem.local>
2022-04-26 20:53:42 -0300

    fix linters errors

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
-import _ from 'lodash';
 import './index.css';
-import './iteration.js';
 import { list } from './iteration.js';
-
 
 const pageContainer = document.querySelector('.page-container');
 const title = document.createElement('h1');
-title.textContent = `Today's To Do`;
+title.textContent = 'Today\'s To Do';
 pageContainer.appendChild(title);
 const input = document.createElement('input');
 pageContainer.appendChild(input);
-input.setAttribute('type','text');
-input.setAttribute('placeholder', "add to your list...");
+input.setAttribute('type', 'text');
+input.setAttribute('placeholder', 'add to your list...');
 pageContainer.appendChild(list);
 const remove = document.createElement('a');
-remove.setAttribute('href','#');
-remove.textContent = 'Clear all completed'
-pageContainer.appendChild(remove)
+remove.setAttribute('href', '#');
+remove.textContent = 'Clear all completed';
+pageContainer.appendChild(remove);
