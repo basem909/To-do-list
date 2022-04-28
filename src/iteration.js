88474b0e672bb0b0313fae609a909dae19fe7638
Bassem <bassem@MacBook-Pro-de-Bassem.local>
2022-04-28 19:20:24 -0300

    fix linters errors in iteration.js

--- src/iteration.js
+++ src/iteration.js
@@ -1,13 +1,12 @@
 import { reload, saveLocal } from './localstorage.js';
 import { removeTask } from './remove.js';
 
-export const list = document.createElement("ul");
+export const list = document.createElement('ul');
 list.classList.add('todo-container');
 export function listItem(tasks) {
   list.innerHTML = '';
   for (let i = 0; i < tasks.length; i += 1) {
     list.innerHTML += `<li class='list-item'><input type="checkbox"><input class ='edit' placeholder = '${tasks[i].description}' id ='${tasks[i].index}' disabled> <button class = "button"id ='${tasks[i].index}'>remove</button></li>`;
-
   }
   const listbtn = document.querySelectorAll('.button');
   listbtn.forEach((btn) => {
@@ -15,24 +14,27 @@ export function listItem(tasks) {
   });
   const editInput = document.querySelectorAll('.edit');
   const clickTarget = document.querySelectorAll('li');
-  clickTarget.forEach((editarea) => {editarea.addEventListener('click',()=>{
-    editarea.style.backgroundColor = 'lightyellow'
-  editInput.forEach((editput)=>{
-    editput.removeAttribute('disabled')
-  }) })})
-
-editInput.forEach((editput)=>{
-  editput.addEventListener('keypress',(e)=>{
-     if (e.key === 'Enter'){
-       editput.setAttribute('disabled','')
-       clickTarget.forEach((editarea)=>{
-         editarea.style.backgroundColor = 'white'
-       })
-       tasks[e.target.id].description = editput.value
-       saveLocal('tasks',tasks)
-       reload();
-     }
+  clickTarget.forEach((editarea) => {
+    editarea.addEventListener('click', () => {
+      editarea.style.backgroundColor = 'lightyellow';
+      editInput.forEach((editput) => {
+        editput.removeAttribute('disabled');
+      });
+    });
   });
-})
+
+  editInput.forEach((editput) => {
+    editput.addEventListener('keypress', (e) => {
+      if (e.key === 'Enter') {
+        editput.setAttribute('disabled', '');
+        clickTarget.forEach((editarea) => {
+          editarea.style.backgroundColor = 'white';
+        });
+        tasks[e.target.id].description = editput.value;
+        saveLocal('tasks', tasks);
+        reload();
+      }
+    });
+  });
   return list;
 }
