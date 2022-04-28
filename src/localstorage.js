ee861d9be5b344ef66715ed884968144aed444c1
Bassem <bassem@MacBook-Pro-de-Bassem.local>
2022-04-28 19:08:49 -0300

    fix linters errors in localstorage.js

--- src/localstorage.js
+++ src/localstorage.js
@@ -1,22 +1,20 @@
 import { listItem } from './iteration.js';
 const saveLocal = (key,value) => {
-    localStorage.setItem(key, JSON.stringify(value));
-}
+  localStorage.setItem(key, JSON.stringify(value));
+};
 
 const getFromLocal = (key) => {
-  const stored = JSON.parse( localStorage.getItem(key))
-  let storedTasks = [];
+  const stored = JSON.parse(localStorage.getItem(key));
+  const storedTasks = [];
   if (stored) {
-    for (let i = 0;i < stored.length ;i += 1) {
-      storedTasks.push(stored[i])
-
-    }
+    for (let i = 0; i < stored.length; i += 1) {
+      storedTasks.push(stored[i]);}
   }
   return storedTasks;
-}
+};
 
 const reload = () => {
   const store = getFromLocal('tasks');
   listItem(store);
 };
-export {saveLocal,getFromLocal,reload};
+export { saveLocal, getFromLocal, reload };
