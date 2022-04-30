7ec064c3d3a985892a08b382b1489459abe08992
Bassem <bassem@MacBook-Pro-de-Bassem.local>
2022-04-29 21:08:27 -0300

    fix linters errors

--- src/iteration.js
+++ src/iteration.js
@@ -1,87 +1,87 @@
-import { saveLocal, getFromLocal } from "./localstorage.js";
+import { saveLocal, getFromLocal } from './localstorage.js';
 
-const _ = require("lodash");
+const _ = require('lodash');
 
-export const remove = document.createElement("a");
-export const list = document.createElement("ul");
-list.classList.add("todo-container");
+export const remove = document.createElement('a');
+export const list = document.createElement('ul');
+list.classList.add('todo-container');
 export function listItem(tasks) {
-  list.innerHTML = "";
+  list.innerHTML = '';
   const reload = () => {
-    const store = getFromLocal("tasks");
+    const store = getFromLocal('tasks');
     listItem(store);
   };
   for (let i = 0; i < tasks.length; i += 1) {
     list.innerHTML += `<li class='list-item'><input id='${
       tasks[i].index
     }' class='check' type="checkbox" ${
-      tasks[i].completed ? "checked" : ""
+      tasks[i].completed ? 'checked' : ''
     } ><input class ='edit' placeholder = '${tasks[i].description}' id ='${
       tasks[i].index
     }' disabled> <button class = "button"id ='${
       tasks[i].index
     }'>remove</button></li>`;
   }
-  const listbtn = document.querySelectorAll(".button");
+  const listbtn = document.querySelectorAll('.button');
   listbtn.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
-      const tasklist = getFromLocal("tasks");
+    btn.addEventListener('click', (e) => {
+      const tasklist = getFromLocal('tasks');
       tasklist.splice(e.target.id - 1, 1);
       for (let i = 0; i < tasklist.length; i += 1) {
         tasklist[i].index = i + 1;
       }
-      saveLocal("tasks", tasklist);
+      saveLocal('tasks', tasklist);
       reload();
       window.location.reload();
     });
   });
-  const editInput = document.querySelectorAll(".edit");
-  const clickTarget = document.querySelectorAll("li");
+  const editInput = document.querySelectorAll('.edit');
+  const clickTarget = document.querySelectorAll('li');
   clickTarget.forEach((editarea) => {
-    editarea.addEventListener("click", () => {
-      editarea.style.backgroundColor = "lightyellow";
+    editarea.addEventListener('click', () => {
+      editarea.style.backgroundColor = 'lightyellow';
       editInput.forEach((editput) => {
-        editput.removeAttribute("disabled");
+        editput.removeAttribute('disabled');
       });
     });
   });
 
   editInput.forEach((editput) => {
-    editput.addEventListener("keypress", (e) => {
-      if (e.key === "Enter") {
-        editput.setAttribute("disabled", "");
+    editput.addEventListener('keypress', (e) => {
+      if (e.key === 'Enter') {
+        editput.setAttribute('disabled', '');
         clickTarget.forEach((editarea) => {
-          editarea.style.backgroundColor = "white";
+          editarea.style.backgroundColor = 'white';
         });
         tasks[e.target.id - 1].description = editput.value;
-        saveLocal("tasks", tasks);
+        saveLocal('tasks', tasks);
         reload();
       }
     });
   });
 
-  const check = document.querySelectorAll(".check");
+  const check = document.querySelectorAll('.check');
   check.forEach((checkbox) => {
-    checkbox.addEventListener("change", (e) => {
+    checkbox.addEventListener('change', (e) => {
       if (tasks[e.target.id - 1].completed === false) {
         tasks[e.target.id - 1].completed = true;
-        saveLocal("tasks", tasks);
+        saveLocal('tasks', tasks);
         reload();
       } else {
         tasks[e.target.id - 1].completed = false;
-        saveLocal("tasks", tasks);
+        saveLocal('tasks', tasks);
         reload();
       }
     });
   });
-  remove.addEventListener("click", () => {
+  remove.addEventListener('click', () => {
     const newtasks = _.filter(tasks, function del(o) {
-      return !o.completed;
+      return !o.completed; 
     });
     for (let i = 0; i < newtasks.length; i += 1) {
       newtasks[i].index = i + 1;
     }
-    saveLocal("tasks", newtasks);
+    saveLocal('tasks', newtasks);
     reload();
     window.location.reload();
   });
@@ -89,6 +89,6 @@ export function listItem(tasks) {
 }
 
 export const reload = () => {
-  const store = getFromLocal("tasks");
+  const store = getFromLocal('tasks');
   listItem(store);
 };
