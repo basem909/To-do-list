5605d68fef09333eaea28b3490f6b18cf228cb32
Bassem <bassem@MacBook-Pro-de-Bassem.local>
2022-05-03 13:54:22 -0300

    move functions to remove.js

--- src/iteration.js
+++ src/iteration.js
@@ -1,6 +1,5 @@
 /* eslint disable */
 import { saveLocal, getFromLocal } from './localstorage.js';
-import { tasks } from './add.js';
 
 const _ = require('lodash');
 
@@ -92,25 +91,4 @@ export const reload = () => {
   listItem(store);
 };
 
-const checkboxChange = (e) => {
-  if (tasks[e.target.id - 1].completed === false) {
-    tasks[e.target.id - 1].completed = true;
-    saveLocal('tasks', tasks);
-    reload();
-  } else {
-    tasks[e.target.id - 1].completed = false;
-    saveLocal('tasks', tasks);
-    reload();
-  }
-};
-checkboxChange;
-const removeCompleted = () => {
-  const newtasks = _.filter(tasks, (o) => !o.completed);
-  for (let i = 0; i < newtasks.length; i += 1) {
-    newtasks[i].index = i + 1;
-  }
-  saveLocal('tasks', newtasks);
-  reload();
-  window.location.reload();
-};
-removeCompleted;
+
--- src/remove.js
+++ src/remove.js
@@ -1,2 +1,25 @@
-const listbtn = document.querySelector('.button');
+import { tasks } from './add.js' 
+  const listbtn = document.querySelector('.button');
 export default listbtn;
+const checkboxChange = (e) => {
+  if (tasks[e.target.id - 1].completed === false) {
+    tasks[e.target.id - 1].completed = true;
+    saveLocal("tasks", tasks);
+    reload();
+  } else {
+    tasks[e.target.id - 1].completed = false;
+    saveLocal("tasks", tasks);
+    reload();
+  }
+};
+checkboxChange();
+const removeCompleted = () => {
+  const newtasks = _.filter(tasks, (o) => !o.completed);
+  for (let i = 0; i < newtasks.length; i += 1) {
+    newtasks[i].index = i + 1;
+  }
+  saveLocal("tasks", newtasks);
+  reload();
+  window.location.reload();
+};
+removeCompleted();
