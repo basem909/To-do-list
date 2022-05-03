75044f7546777c303304996ef919c69045a5683d
Bassem <bassem@MacBook-Pro-de-Bassem.local>
2022-05-03 13:14:08 -0300

    fix linters issues

--- src/iteration.js
+++ src/iteration.js
@@ -8,11 +8,11 @@ list.classList.add('todo-container');
 const checkboxChange = (e) => {
   if (tasks[e.target.id - 1].completed === false) {
     tasks[e.target.id - 1].completed = true;
-    saveLocal("tasks", tasks);
+    saveLocal('tasks', tasks);
     reload();
   } else {
     tasks[e.target.id - 1].completed = false;
-    saveLocal("tasks", tasks);
+    saveLocal('tasks', tasks);
     reload();
   }
 };
@@ -21,7 +21,7 @@ const removeCompleted = () => {
   for (let i = 0; i < newtasks.length; i += 1) {
     newtasks[i].index = i + 1;
   }
-  saveLocal("tasks", newtasks);
+  saveLocal('tasks', newtasks);
   reload();
   window.location.reload();
 };
@@ -34,11 +34,11 @@ export function listItem(tasks) {
   for (let i = 0; i < tasks.length; i += 1) {
     list.innerHTML += `<li class='list-item'><input id='${
       tasks[i].index
-    }' class='check' type="checkbox" ${
+    }' class='check' type='checkbox' ${
       tasks[i].completed ? 'checked' : ''
     } ><input class ='edit' placeholder = '${tasks[i].description}' id ='${
       tasks[i].index
-    }' disabled> <button class = "button"id ='${
+    }' disabled> <button class = 'button'id ='${
       tasks[i].index
     }'>remove</button></li>`;
   }
@@ -82,14 +82,24 @@ export function listItem(tasks) {
 
   const check = document.querySelectorAll('.check');
   check.forEach((checkbox) => {
-    checkbox.addEventListener('change', checkboxChange);
+    checkbox.addEventListener('change', (e) => {
+      if (tasks[e.target.id - 1].completed === false) {
+        tasks[e.target.id - 1].completed = true;
+        saveLocal('tasks', tasks);
+        reload();
+      } else {
+        tasks[e.target.id - 1].completed = false;
+        saveLocal('tasks', tasks);
+        reload();
+      }
+    });
   });
   remove.addEventListener('click', () => {
   const newtasks = _.filter(tasks, (o) => !o.completed);
   for (let i = 0; i < newtasks.length; i += 1) {
     newtasks[i].index = i + 1;
   }
-  saveLocal("tasks", newtasks);
+  saveLocal('tasks', newtasks);
   reload();
   window.location.reload();
 });
